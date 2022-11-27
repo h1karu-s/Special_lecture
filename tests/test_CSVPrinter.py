7ab0727101c5677493891c77e3e29f9ec838103e
hikaru-sh <polaris.2.71828@gmail.com>
2022-11-27 14:25:09 +0900

    add

--- tests/test_CSVPrinter.py
+++ tests/test_CSVPrinter.py
@@ -1,5 +1,16 @@
 import unittest
-from CSVPrinter import CSVPrinter
+import csv
+# from CSVPrinter import CSVPrinter
+
+class CSVPrinter:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def read(self):
+        with open(self.file_name) as f:
+            reader = csv.reader(f)
+            lines = [row for row in reader]
+        return lines
 
 
 class TestCSVPrinter(unittest.TestCase):
